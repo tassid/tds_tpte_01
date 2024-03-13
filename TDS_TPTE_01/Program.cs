60359561a45db35595c56e2cecb9fb06a8321601
tassid <tassianeanzolin@alunos.utfpr.edu.br>
2024-03-12 21:06:54 -0300

    try fix Non-nullable property 'Name' must contain a non-null value when exiting constructor. Consider declaring the property as nullable.

--- TDS_TPTE_01/Program.cs
+++ TDS_TPTE_01/Program.cs
@@ -115,8 +115,8 @@ class Program
 record Product
 {
     public int Id { get; init; }
-    public string Name { get; init; }
-    public string Description { get; init; }
+    public string? Name { get; init; }
+    public string? Description { get; init; }
     public decimal Price { get; init; }
     public int Quantity { get; init; }
 }
